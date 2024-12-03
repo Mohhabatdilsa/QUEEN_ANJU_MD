98483f66b334e7f89a7efd4a6750c406b0c38fb9
Mohhabatdilsa <108210774+Mohhabatdilsa@users.noreply.github.com>
2024-12-03 13:48:31 +0530

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=DYlXEA6J#EWEVEQYWOuF7mK256qP8wnxGJIQFS-RCtLIbiNh0QGo",
 };
